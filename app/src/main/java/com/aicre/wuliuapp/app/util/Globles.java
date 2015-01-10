d83206b070b0d5a2ace1be719d062cd988b76b80
Owen <weihongxin@hustascii.com>
2015-01-10 21:36:06 +0800

    register

--- app/src/main/java/com/aicre/wuliuapp/app/util/Globles.java
+++ app/src/main/java/com/aicre/wuliuapp/app/util/Globles.java
@@ -10,14 +10,16 @@ public class Globles {
 
     public static String LOGIN_URL = TEST_URL+"user_login.php";
 
-    public static String INFOLIST_URL = BASE_URL+"info_list.php";
+    public static String INFOLIST_URL = TEST_URL+"info_list.php";
 
     public static String USERINFO_URL = BASE_URL+"user_info.php";
+
+    public static String GET_CODE = TEST_URL+"cfword.php";
 
 
     public static String ADDINFO_URL = BASE_URL+"add_info.php";
 
-    public static String REGISTER_URL = BASE_URL+"register.php";
+    public static String REGISTER_URL = TEST_URL+"user_register.php";
 
     public static String UPLOAD_PIC = TEST_URL+"uppic.php";
 
